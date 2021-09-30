7bfc622818c73fc818b3df24ef26a73a8997b495
npaillasson <nathanpaillasson3@gmail.com>
2021-09-30 14:15:52 +0200

    update views.py

--- src/api/views.py
+++ src/api/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from .models import Comments, Contributors, Issues, Projects
 from .serializer import CommentsSerializer, ContributorsSerializer, IssuesSerializer, ProjectsSerializer
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def project_list(request):
 
     if request.method == 'GET':
         projetcs = Projects.objects.all()
         serializer = ProjectsSerializer(projetcs, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        serializer = ProjectsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)

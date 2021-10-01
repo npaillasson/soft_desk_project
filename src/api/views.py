5e1cdfa08c5799c11385fbb200e8191cfb2747ef
npaillasson <nathanpaillasson3@gmail.com>
2021-10-01 16:56:21 +0200

    ProjectList optimization

--- src/api/views.py
+++ src/api/views.py
@@ -1,26 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.views import APIView
-from rest_framework.parsers import JSONParser
+from rest_framework import mixins, generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Comments, Contributors, Issues, Projects
 from .serializer import CommentsSerializer, ContributorsSerializer, IssuesSerializer, ProjectsSerializer
 
 
-class ProjectList(APIView):
+class ProjectList(generics.ListCreateAPIView):
     permission_classes = ([IsAuthenticated])
-
-    def get(self, request, format=None):
-        projetcs = Projects.objects.all()
-        serializer = ProjectsSerializer(projetcs, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    def post(self, request, format=None):
-        serializer = ProjectsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer

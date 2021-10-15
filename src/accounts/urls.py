3f5ceced4baa113f61ffb845e805851e97cbf563
npaillasson <nathanpaillasson3@gmail.com>
2021-10-15 18:05:06 +0200

    Errors in case of not strong enough passwords are displayed properly

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -5,5 +5,13 @@ from .views import UserCreate
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("signup/", UserCreate.as_view(), name="new_account"),
+    path(
+        "signup/",
+        UserCreate.as_view(
+            {
+                "post": "create",
+            }
+        ),
+        name="new_account",
+    ),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,30 @@
-from rest_framework import generics
+from django.core import exceptions
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import status, viewsets
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
+from .models import User
 
 
-class UserCreate(generics.CreateAPIView):
+class UserCreate(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        try:
+            validate_password(serializer.validated_data["password"])
+        except exceptions.ValidationError as exception_to_return:
+            exception_to_return = ", ".join(exception_to_return)
+            detail = {"password": "{}".format(exception_to_return)}
+            raise ValidationError(detail=detail)
+        else:
+            user = User.objects.create(
+                username=serializer.validated_data["username"],
+                email=serializer.validated_data["email"],
+                first_name=serializer.validated_data["first_name"],
+                last_name=serializer.validated_data["last_name"],
+            )
+            user.set_password(serializer.validated_data["password"])
+            user.save()
